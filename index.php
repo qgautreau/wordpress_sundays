8430e21f1643d7f9dd752a2bc26fb17410677d7f
Quentin Gautreau <qgautreau@gmail.com>
2017-05-23 14:33:23 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,220 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta>
+    <title>SUNDAYS</title>
+    <link rel="stylesheet" href="static/external/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="static/css/style.css">
+    <link rel="stylesheet" href="static/external/font-awesome-4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <ul class="nav navbar-nav">
+                    <li><a href="">catalog</a></li>
+                    <li><a href="">about us</a></li>
+                    <li><a href="">stories</a></li>
+                    <li><a href="">blog</a></li>
+                </ul>
+                <form class="navbar-form navbar-right">
+                    <i class="fa fa-search" aria-hidden="true"></i>
+                   <div class="form-group" hidden="true">
+                     <input type="text" class="form-control" placeholder="Search">
+                     <button type="submit" class="btn btn-default">Submit</button>
+                   </div>
+                </form>
+            </div>
+        </nav>
+        <section>
+            <h1>sundays</h1>
+            <p>we craft ocean friendly apparel</p>
+            <button class="btn btn-default tocatalog">browse our products</button>
+        </section>
+    </header>
+    <main>
+        <h2>spring summer 2017</h2>
+        <div class="container">
+
+            <article>
+                <img src="http://lorempixel.com/120/150" alt="product" class="img-responsive">
+                <p class="status">buy</p>
+                <p>winter scarf</p>
+                <p>$180.00</p>
+            </article>
+
+            <article>
+                <img src="http://lorempixel.com/120/150" alt="product" class="img-responsive">
+                <p class="status">on sale</p>
+                <p>summer sandals</p>
+                <p>$220.00</p>
+            </article>
+
+            <article>
+                <img src="http://lorempixel.com/120/150" alt="product" class="img-responsive">
+                <p class="status">buy</p>
+                <p>red hat and worf</p>
+                <p>$200.00</p>
+            </article>
+
+            <article>
+                <img src="http://lorempixel.com/120/150" alt="product" class="img-responsive">
+                <p class="status">buy</p>
+                <p>green coat</p>
+                <p>$220.00</p>
+            </article>
+
+            <article>
+                <img src="http://lorempixel.com/120/150" alt="product" class="img-responsive">
+                <p class="status">on sale</p>
+                <p>dark coat</p>
+                <p>$220.00</p>
+            </article>
+
+            <article>
+                <img src="http://lorempixel.com/120/150" alt="product" class="img-responsive">
+                <p class="status">buy</p>
+                <p>blue trousers</p>
+                <p>$220.00</p>
+            </article>
+
+            <article>
+                <img src="http://lorempixel.com/120/150" alt="product" class="img-responsive">
+                <p class="status">sold out</p>
+                <p>blue sweater</p>
+                <p>s$220.00</p>
+            </article>
+
+            <article>
+                <img src="http://lorempixel.com/120/150" alt="product" class="img-responsive">
+                <p class="status">buy</p>
+                <p>blue shirt</p>
+                <p>$220.00</p>
+            </article>
+
+            <article>
+                <img src="http://lorempixel.com/120/150" alt="product" class="img-responsive">
+                <p class="status">on sale</p>
+                <p>blue backpack</p>
+                <p>$120.00</p>
+            </article>
+
+            <article>
+                <img src="http://lorempixel.com/120/150" alt="product" class="img-responsive">
+                <p class="status">on sale</p>
+                <p>beach chair</p>
+                <p>$80.00</p>
+            </article>
+
+            <article>
+                <img src="http://lorempixel.com/120/150" alt="product" class="img-responsive">
+                <p class="status">on sale</p>
+                <p>decoration letter</p>
+                <p>$120.00</p>
+            </article>
+
+            <article>
+                <img src="http://lorempixel.com/120/150" alt="product" class="img-responsive">
+                <p class="status">on sale</p>
+                <p>sutton bench</p>
+                <p>$120.00</p>
+            </article>
+
+            <article>
+                <img src="http://lorempixel.com/120/150" alt="product" class="img-responsive">
+                <p class="status">buy</p>
+                <p>red trousers</p>
+                <p>$220.00</p>
+            </article>
+
+            <article>
+                <img src="http://lorempixel.com/120/150" alt="product" class="img-responsive">
+                <p class="status">on sale</p>
+                <p>winter sandals</p>
+                <p>$220.00</p>
+            </article>
+
+            <article>
+                <img src="http://lorempixel.com/120/150" alt="product" class="img-responsive">
+                <p class="status">on sale</p>
+                <p>beach chair</p>
+                <p>$80.00</p>
+            </article>
+
+            <article>
+                <img src="http://lorempixel.com/120/150" alt="product" class="img-responsive">
+                <p class="status">buy</p>
+                <p>winter scarf</p>
+                <p>$180.00</p>
+            </article>
+
+            <article>
+                <img src="http://lorempixel.com/120/150" alt="product" class="img-responsive">
+                <p class="status">on sale</p>
+                <p>green backpack</p>
+                <p>$120.00</p>
+            </article>
+
+            <article>
+                <img src="http://lorempixel.com/120/150" alt="product" class="img-responsive">
+                <p class="status">sold out</p>
+                <p>navy sweater</p>
+                <p>$220.00</p>
+            </article>
+        </div>
+    </main>
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-4 col-md-3 col-lg-3">
+                    <section>
+                        <nav id="quicklinks">
+                            <h4>quick links</h4>
+                            <li><a href="">about us</a></li>
+                            <li><a href="">blog</a></li>
+                            <li><a href="">careers</a></li>
+                        </nav>
+                    </section>
+                </div>
+                <div class="col-xs-12 col-sm-4 col-md-3 col-lg-3">
+                    <section>
+                        <h4>contact us</h4>
+                        <nav id="contact">
+                            <li><i class="fa fa-twitter" aria-hidden="true"></i><a href="http://twitter.com" target="_blank">@californiatheme</a></li>
+                            <li><i class="fa fa-envelope-o" aria-hidden="true"></i>
+                            <a href="#">support@smallvictories.co</a></li>
+                            <li><i class="fa fa-phone" aria-hidden="true"></i> +1 917 (123) 4567</li>
+                            </nav>
+                    </section>
+                </div>
+                <div class="col-xs-12 col-sm-4 col-md-3 col-lg-3">
+                    <section>
+                        <h4>newsletter</h4>
+                        <input type="text" placeholder="You email...">
+                        <button>ok</button>
+                    </section>
+                </div>
+                <div class="col-xs-12 col-sm-4 col-md-3 col-lg-3">
+                    <section>
+                        <h4>follow us</h4>
+                        <nav id="socialnav">
+                            <li><i class="fa fa-pinterest-square" aria-hidden="true"></i>
+                            <a href="http://pinterest.com" target="_blank">pinterest</a></li>
+                            <li><i class="fa fa-instagram" aria-hidden="true"></i>
+                            <a href="http://instagram.com" target="_blank">instagram</a></li>
+                            <li><i class="fa fa-vimeo-square" aria-hidden="true"></i>
+                            <a href="http://vimeo.com" target="_blank">vimeo</a></li>
+                        </nav>
+                    </section>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script src="static/external/jquery/dist/jquery.min.js"></script>
+    <script src="static/js/script.js"></script>
+</body>
+
+</html>
---
+++ static/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    if($(".status").html() == "BUY"){
+        $(".status").css("color", "green");
+        console.log("plop");
+    }
+    console.log("plop");
+});
